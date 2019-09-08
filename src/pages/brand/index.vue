c118698db3bd32c4d1ac53e61f741d02286bf200
SupStrong <1458380717@qq.com>
2019-09-08 21:11:09 +0800

    gengxin

--- src/pages/brand/index.vue
+++ src/pages/brand/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="content"> 
+  <div class="content">
+    <div class="search">
+      <div class="search-main">
+          <input type="text" v-model="searchText" placeholder="搜索您需要的品牌名" confirm-type='search'  @confirm="confirm($event)" @focus="searchChanges" @input="searchChanges" class="search-inp">
+          <i @click.stop="closeSearch" v-if="searchChange" class="iconfont iconshanchu"></i>
+      </div>
+    </div>
       <view class="container">
         <!--左侧栏-->
         <view class="nav_left">
@@ -10,16 +16,18 @@
         <!--右侧栏-->
         <view class="nav_right">
           <!--如果有数据，才遍历项-->
-          <view v-if="cateItems[curIndex].children">
-            <block v-for="(item,index) of cateItems[curIndex].children" :key="index">
-              <view class="nav_right_items">
-              <!--界面跳转 -->
-                  <text>{{item.title}}</text>
-              </view>
-            </block>
-          </view>
-          <!--如果无数据，则显示数据-->
-          <view class="nodata_text" v-else>该分类暂无数据</view>
+          <div class="listBrand">
+              <div
+                class="brand"
+                v-for="(item,index) in cateItems[curIndex].children"
+                :key="index"
+                @click="setRouter('/pages/brand/details/main',item.id)"
+              >
+                <img class="img" :src="item.logo" alt>
+                <div class="brandName G-one-cloum">{{item.name}}</div>
+              </div>
+          </div>
+
         </view>
       </view>
   </div>
@@ -38,10 +46,92 @@ export default {
   data() {
     return {
       cateItems :[   
-      {id:"1",title:'政策1',children:[{id:1,title:"hello1",childrenId:11}]},
-      {id:"2",title:'政策2',children:[{id:2,title:"hello2",childrenId:22}]},
-      {id:"3",title:'政策3',children:[{id:3,title:"hello3",childrenId:33}]},
-      {id:"4",title:'政策4',children:[{id:4,title:"hello4",childrenId:44}]}],
+      {id:"1",title:'小狗',children:[
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+         {logo:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg',name:"狗大脸"},
+
+      ]},
+      {id:"2",title:'小猫',children:[
+        {logo:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3389412202,3441609535&fm=26&gp=0.jpg',name:"猫小脸"}
+      ]},
+      {id:"3",title:'小猪',children:[
+        {logo:'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=4135477902,3355939884&fm=26&gp=0.jpg',name:"猪圆脸"}
+      ]},
+      {id:"4",title:'小羊',children:[
+        {logo:'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1176078931,2627391218&fm=26&gp=0.jpg',name:"羊尖脸"}         
+      ]}],
       curIndex:0,
     };
   },
@@ -56,10 +146,51 @@ export default {
 </script>
 <style lang="scss" scoped>
 
-page{  
+.content{  
   background: #ddd;  
-  height: 100%;
-}  
+} 
+.search {
+	width: 100%;
+  height: .44rem;
+  display: -webkit-flex;
+  align-items: center;
+  background-color: #fff;
+  border-bottom: 1px solid #ececec;
+  .search-main{
+    display:-webkit-flex;
+    align-items:center;
+    background-color:#f6f6f6;
+    position: relative;
+    width: 3.55rem;
+    margin:0 auto;
+    height: .32rem;
+    line-height: .32rem;
+    padding:.1rem;
+    border-radius: .05rem;
+    overflow: hidden;
+    .iconfont{
+      font-size: .16rem;
+      opacity: 0.2;
+      position: absolute;
+      right: .1rem;
+      z-index: 999;
+    }
+  }
+  .search-inp{
+    width: 100%;
+    height: .32rem;
+    line-height: .32rem;
+    background-color: #f6f6f6;
+    color: #333;
+    font-size: .14rem;
+  }
+  .search-close{
+    color: #f36e20;
+    font-size: .14rem;
+    margin:0 .1rem;
+  }
+}
+
 /*总体主盒子*/  
 .container {  
   position: relative;  
@@ -79,6 +210,7 @@ page{
   position: absolute;
   top: 0;
   left: 0; 
+  margin-top:.05rem;
 }  
 /*左侧栏list的item*/  
 .nav_left .nav_left_items{  
@@ -104,10 +236,12 @@ page{
   flex: 1;  
   /*宽度75%，高度占满，并使用百分比布局*/  
   width: 75%;  
-  height: 1000px;  
+  height:calc(100vh - 100rpx);
+  overflow:scroll;
   padding: 10px;  
   box-sizing: border-box;  
   background:#fff;  
+  margin-top:.05rem;
 }  
 /*右侧栏list的item*/  
 .nav_right .nav_right_items{  
@@ -143,4 +277,59 @@ page{
   font-size: 14px;  
   text-align: center;  
 }
+
+.listBrand {
+  display: -webkit-flex;
+  flex-wrap: wrap;
+  height:auto;
+  overflow:scroll;
+  .brand {
+    width: calc(100% / 3);
+    display: -webkit-flex;
+    background-color: #fff;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    position: relative;
+    padding-top:.05rem;
+    .img {
+      max-width: 0.75rem;
+      max-height: 0.75rem;
+    }
+
+    .brandName {
+      margin-top: 0.1rem;
+      font-size: 0.14rem;
+    }
+  }
+}
+
+.invalid {
+  width: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 1.83rem;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: -webkit-flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff !important;
+  font-weight: normal;
+}
+.is {
+  width: 100%;
+  height: 0.44rem;
+}
+.nothing {
+  width: 100%;
+  justify-content: center;
+  display: -webkit-flex;
+  color: #666;
+  align-items: center;
+  font-size: 0.16rem;
+  margin-top: 0.6rem;
+}
+
 </style>
